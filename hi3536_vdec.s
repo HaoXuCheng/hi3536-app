cc78f17107b4c7171282bea9bcbbb2399e51bbf5
黄志敏 <huangzhimin@techjt.cn>
2021-12-17 16:46:06 +0800

    允许保存视频输出格式
    赵工确认可以支持不同的输出格式

--- hi3536_vdec.s
+++ hi3536_vdec.s
@@ -3,7 +3,7 @@
   <BgColor type="int" discrete="{0x0,0xFF,0x8000}" alias="black,blue,green" save="true"/>
   <inst>
     <vo>
-      <IntfSync type="int" discrete="{7,8,9,10,11,12,30,31,32}" alias="720p50,720p60,1080i50,1080i60,1080p50,1080p60,4kp30,4kp50,4kp60" cond="rerun"/>
+      <IntfSync type="int" discrete="{7,8,9,10,11,12,30,31,32}" alias="720p50,720p60,1080i50,1080i60,1080p50,1080p60,4kp30,4kp50,4kp60" cond="rerun" save="true"/>
       <IntfType type="int" discrete="{0x10,0x20}" alias="bt1120,hdmi" const="true"/>
       <mode type="int" discrete="{0,1}" alias="1x1,2x2" save="true"/>
       <channel type="int" discrete="{0,1,2,3}" save="true"/>
